3cd9899334e5cdf0b2de5eb2616118ba42255bba
Anton Loskutov <mrloskutov@live.ru>
2022-10-01 18:23:21 +0300

    Initial Commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Антон Лоскутов on 01.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
