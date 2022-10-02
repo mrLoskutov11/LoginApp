31d9ea14b1c0ee8b4660a7b10e7a4c59d4fe937d
Anton Loskutov <mrloskutov@live.ru>
2022-10-02 15:59:47 +0300

    Create Login View

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userName: UITextField!
+    @IBOutlet var password: UITextField!
+    @IBOutlet var logInButton: UIButton!
+    @IBOutlet var forgotUserNameButton: UIButton!
+    @IBOutlet var forgotPasswordButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
 }
-
